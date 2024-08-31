ff5efcab09b99968b2778fa5e82292fbca554b4a
Hanhtunaung97 <littlehanhtunaung97>
2024-08-31 20:59:38 +0800

    loader & nav feature fix

--- src/components/Loader.components.jsx
+++ src/components/Loader.components.jsx
@@ -1,11 +1,20 @@
 import { Html } from "@react-three/drei";
 import React from "react";
+import { spiral } from "ldrs";
 
 const LoaderComponents = () => {
+  spiral.register();
+
   return (
     <Html>
       <div className="flex justify-center items-center">
-        <div className=" w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin"></div>
+        {/* <div className=" w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin"></div> */}
+        <l-spiral
+          size="40"
+          speed="0.7"
+          color="blue"
+          className="w-20 h-20 "
+        ></l-spiral>
       </div>
     </Html>
   );
--- src/components/Navbar.components.jsx
+++ src/components/Navbar.components.jsx
@@ -7,19 +7,32 @@ const NavbarComponents = () => {
       <NavLink
         to={"/"}
         className={
-          "w-10 h-10 bg-white shadow-md flex justify-center items-center rounded-lg font-bold font-heading hover:-translate-y-0.5 hover:shadow-xl hover:scale-105 transition-all"
+          "w-10 h-10 bg-white shadow-md flex justify-center items-center rounded-lg font-bold font-heading hover:-translate-y-0.5 hover:shadow-md hover:scale-105 transition-all"
         }
       >
         <p className="blue-gradient_text">TH</p>
       </NavLink>
       <nav className=" flex gap-7 text-lg font-medium font-heading">
-        <NavLink to="/about" className={({isActive}) =>isActive? "text-blue-500":" text-black" }>
-          <p>About</p>
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : " text-black"
+          }
+        >
+          <p className="hover:-translate-y-0.5 hover:scale-105 transition-all">
+            About
+          </p>
         </NavLink>
-        <NavLink to="/projects" className={({isActive}) =>isActive? "text-blue-500":" text-black" }>
-          <p>Projects</p>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : " text-black"
+          }
+        >
+          <p className="hover:-translate-y-0.5 hover:scale-105 transition-all">
+            Projects
+          </p>
         </NavLink>
-
       </nav>
     </header>
   );
